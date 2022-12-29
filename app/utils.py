d7cd7e6263110788ff82848ce799248908ce2deb
Gautam G <gautamvignesh@gmail.com>
2022-12-28 18:37:16 -0600

    feat: utils

---
+++ app/utils.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+replacements = {}
+
+
+@st.cache
+def cache_input(df):
+    return df
+
+
+@st.experimental_memo
+def convert_df(df):
+    return df.to_csv(index=False).encode('utf-8')
+
+
+def pair_replacements():
+    for i, word in enumerate(st.session_state.user_text_input):
+        if not st.session_state[f"Replacement_{i}"]:
+            st.error("replacement cannot be blank")
+            st.stop()
+        replacements[st.session_state.user_text_input[i]] = st.session_state[f"Replacement_{i}"]
